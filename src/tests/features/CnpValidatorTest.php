2987d5d40a4a61eecd8131bcf5cd57d031ba9e45
mishu <mishu@xtelecom.ro>
2017-06-07 11:17:23 +0300

    fix

--- src/tests/features/CnpValidatorTest.php
+++ src/tests/features/CnpValidatorTest.php
@@ -7,15 +7,12 @@ class CnpValidatorTest extends TestCase
     /** @test */
     public function validate_cnp()
     {
-        // Arrange
         $goodCnp = '1800119081824';
         $badCnp = '1800191081823';
 
-        // Act
         $goodResult = \Validator::make(['cnp' => $goodCnp], ['cnp' => 'cnp']);
         $badResult = \Validator::make(['cnp' => $badCnp], ['cnp' => 'cnp']);
 
-        // Assert
         $this->assertTrue(!$goodResult->fails());
         $this->assertTrue($badResult->fails());
     }
