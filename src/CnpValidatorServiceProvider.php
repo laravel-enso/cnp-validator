c850e869c63c464049daab6a4c62bd974164ed86
mishu <mishu@xtelecom.ro>
2017-06-07 12:12:14 +0300

    removed useless comments

--- src/CnpValidatorServiceProvider.php
+++ src/CnpValidatorServiceProvider.php
@@ -6,21 +6,11 @@ use Illuminate\Support\ServiceProvider;
 
 class CnpValidatorServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         \Validator::extend('cnp', 'LaravelEnso\CnpValidator\app\Classes\Validations@validatorCnp');
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
     public function register()
     {
         //
